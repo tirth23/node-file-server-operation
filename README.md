c3f93ee45ac7ba5fe5513facc26790dc8a85d07f
Tirth Patel <tirthpatel23t@gmail.com>
2024-10-29 19:28:12 +0530

    update

--- README.md
+++ README.md
@@ -8,3 +8,35 @@
 Event-Driven: Node.js operates based on events, responding to triggers or signals that tell it to perform specific actions. Rather than sequentially executing tasks, Node.js listens for events, like a network request or file operation, and reacts when they happen. This is managed by an event loop, which continually monitors and handles events in a queue.
 
 Non-Blocking: In traditional server-side architectures, if a task is in progress (like reading a file), other tasks are blocked from execution until it completes. Node.js, however, is designed to avoid this blocking by using asynchronous calls. For example, if Node.js needs to read a file, it sends the request and then moves on to handle other tasks, coming back to the file when the read is complete. This way, it doesn’t get "stuck" waiting for a task to finish before moving on.
+
+### When performing heavy I/O operations, such as reading and writing files, making network requests, or interacting with databases, Node.js can initiate these operations and continue executing other code while waiting for the I/O operations to complete. This asynchronous approach is highly advantageous for scenarios with many concurrent I/O tasks.
+
+### In situations involving heavy I/O, it's common for multiple clients to make simultaneous requests to the server. Node.js's non-blocking model allows it to handle a large number of concurrent connections efficiently, making it a suitable choice for scalable applications. It can process incoming requests as soon as they arrive, rather than waiting for each request to complete before moving on to the next one.
+
+### Node.js has a relatively small memory footprint compared to some other web server technologies. This makes it well-suited for small server applications where resource utilization needs to be efficient. You can run multiple Node.js instances on a single server without consuming excessive system resources.
+
+## Server Side Development
+
+### Server-side development, client-side development, and working with database clients are essential components of modern web application development
+### Server-side development refers to the part of web application development that occurs on the server, typically using server-side technologies and programming languages 
+### Server: A server is a computer or software application that responds to client requests over a network. In web development, a server typically hosts the backend of a web application.
+### Server-Side Technologies: Common server-side technologies include Node.js, Python (with frameworks like Django or Flask), Ruby (with Ruby on Rails), Java (with Spring or Java EE), PHP, and more. These technologies enable you to create the server logic, handle requests from clients, interact with databases, and generate dynamic content.
+### Server Logic: Server-side code manages user authentication, business logic, data processing, and database interactions. It often generates HTML, JSON, or other data to send back to the  client.
+### Security: Security measures like input validation, authentication, authorization, and protecting against common vulnerabilities (e.g., SQL injection, XSS) are typically implemented on the server side.
+
+## Client Side Development
+
+### Client-side development focuses on the part of web application development that occurs in the user's web browser.
+### Client: The client is the user's device (e.g., web browser) that sends requests to a server to access web content or services.
+### Client-Side Technologies: Common client-side technologies include HTML, CSS, and JavaScript. HTML is used for structuringweb content, CSS for styling, and JavaScript for adding interactivity and functionality to web pages.
+ 
+## Database Client
+
+### A database client is a software component or library that allows your server-side code to communicate with a database management system (DBMS)
+### Database Management System (DBMS): A DBMS is software that manages databases, including storing, retrieving, updating, and organizing data. Examples of DBMSs include MySQL, PostgreSQL, MongoDB, and SQLite.
+### Database Client Libraries: To interact with a DBMS, developers use specific client libraries or drivers for their chosen programming language. These libraries provide functions and methods to connect to the database, execute queries, and retrieve results.
+### ORM (Object-Relational Mapping): Some server-side frameworks and languages offer ORMs (e.g., Sequelize for Node.js, Hibernate for Java) that provide a higher-level abstraction for working with databases. ORMs map database tables to objects in code, simplifying database interactions. ORM Models data using classes and tables.
+### ODM: Models data using schemas and documents.Example of ODM (Mongoose for MongoDB)
+### Data Access: Server-side code uses database clients to perform CRUD operations (Create, Read, Update, Delete) on data stored in databases. This includes querying data, inserting new records, updating existing records, and deleting records.
+
+###
--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-/* fs module is used to read and write files */
+/* 
+fs module is used to read and write files 
+
+The fs module in Node.js stands for "File System," and it provides a way to work with the file system on your computer or server.
+It allows you to read from and write to files, manipulate directories, perform file operations, and more
+
+Most fs module functions come in both synchronous and asynchronous versions.
+The asynchronous versions (e.g., fs.readFile()) allow non-blocking file operations,
+while synchronous versions (e.g., fs.readFileSync()) block the Node.js event loop until the operation is complete.
+
+Asynchronous methods are typically preferred in Node.js to maintain the application's responsiveness.
+
+node follows error first callback
+(error, data) => { if(error) {} else {} }
+*/
+
 // const fs = require("fs");
 
 // /* read content from file, gives error if not exists */
-// /* node follows error first callback
-// (error, data) => { if(error) {} else {} } */
-
 // fs.readFile("file.txt", "utf-8", (err, data) => {
 // 	if (err) {
 // 		console.log("error", err);
@@ -41,7 +53,7 @@
 
 /* --------------------------------------------------------------------------------------------------------------------------------- */
 
-/* delete file if exists, gives error if not exists */
+// /* delete file if exists, gives error if not exists */
 // fs.unlink("example2.txt", (err) => {
 // 	if (err) {
 // 		console.log("error", err);
@@ -103,54 +115,102 @@
 
 /* --------------------------------------------------------------------------------------------------------------------------------- */
 
-// const path = require("path")
-
-// const fullPath = path.join("folder","subfolder","file.txt")
-// console.log(fullPath)
-
-// const absolutePath = path.resolve("folder","subfolder","file.txt")
-// console.log(absolutePath)
-
-// const fileName = path.basename('./dir/filename.txt')
-// console.log(fileName)
-
-// const dirName = path.dirname('/path/to/filename.txt')
-// console.log(dirName)
-
-// const pathInfo = path.parse('/path/to/filename.txt')
-// console.log(pathInfo)
-
-// /* .. means one level up which means path */
-// const normalizePath = path.normalize("/path/to/../filename.txt")
-// console.log(normalizePath)
+// /* fs.existsSync() is a synchronous method, so it can block the Node.js event loop.
+// an asynchronous approach, fs.access() method */
+// const filePath = "./dir";
+// fs.access(filePath, fs.constants.F_OK, (err) => {
+// 	if (err) {
+// 		console.log(`The path "${filePath}" does not exist.`);
+// 	} else {
+// 		console.log(`The path "${filePath}" exists.`);
+// 	}
+// });
 
 /* --------------------------------------------------------------------------------------------------------------------------------- */
 
-/* copying from one file to another */
-const fs = require("fs");
+/* 
+The path module in Node.js provides utilities for working with file and directory paths. 
+It's an essential module when dealing with file system operations and path manipulation in your Node.js applications.
 
-const sourceFilePath = "./dir/file1.txt";
-const destinationFilePath = "./file2.txt";
+File path formats differ between operating systems. 
+For instance, Windows uses backslashes (\) while POSIX (Linux, macOS) uses forward slashes (/). 
+The path module abstracts these differences, allowing you to write code that works across all platforms without
+worrying about the underlying file system differences. 
+*/
 
-/* create a readable stream */
-const readStream = fs.createReadStream(sourceFilePath);
+const path = require("path")
 
-/* create a writable stream */
-const writeStream = fs.createWriteStream(destinationFilePath);
+/* 
+joins multiple path segments into a single path string, taking care of platform-specific path separators 
+(e.g., backslashes on Windows and forward slashes on Unix-like systems) 
+*/
+// const fullPath = path.join("folder","subfolder","file.txt")
+// console.log(fullPath)
 
-/* pipe the data */
-readStream.pipe(writeStream);
+// /* Resolves an absolute path from multiple path segments, starting from the root directory */
+// const absolutePath = path.resolve("folder","subfolder","file.txt")
+// console.log(absolutePath)
 
-readStream.on("error", (err) => {
-	console.log("Error reading file", err.message);
-});
+// /* Return the last portion of a path. Often used to extract the file name from a fully qualified path */
+// const fileName = path.basename('./dir/filename.txt')
+// console.log(fileName)
 
-readStream.on("end", () => {
-	console.log("File reading completed");
-});
+// /* Returns the directory name of a path */
+// const dirName = path.dirname('/path/to/filename.txt')
+// console.log(dirName)
 
-writeStream.on("error", (err) => {
-	console.log("Error writing file", err);
-});
+// /* Parses a path string into an object with properties like root, dir, base, name, and ext */
+// const pathInfo = path.parse('/path/to/filename.txt')
+// console.log(pathInfo)
+
+// /* .. means one level up which means path. Normalizes a path by resolving '..' and '.' segments and converting
+// slashes to the appropriate platform format */
+// const normalizePath = path.normalize("/path/to/../filename.txt")
+// console.log(normalizePath)
+
+// /* Checks if a path is an absolute path. */
+// const isAbsolute = path.isAbsolute('/path/to/file.txt');
+// console.log(isAbsolute)
+
+/* --------------------------------------------------------------------------------------------------------------------------------- */
+
+/* 
+copying from one file to another 
+
+Streams are collections of data that might not be available all at once and don't have to fit in memory. 
+They allow handling of reading/writing files, network communications, or any kind of end-to-end information exchange in an efficient way
+
+Like in video streams, A portion of the video is buffered (pre-loaded) and played while the next portion is being downloaded. 
+This approach ensures a smooth viewing experience without requiring the entire file to be downloaded first.
+*/
+// const fs = require("fs");
+
+// const sourceFilePath = "./dir/file1.txt";
+// const destinationFilePath = "./file2.txt";
+
+// /* create a readable stream */
+// const readStream = fs.createReadStream(sourceFilePath);
+
+// /* create a writable stream */
+// const writeStream = fs.createWriteStream(destinationFilePath);
+
+// /* 
+// pipe the data from the source stream to the destination stream, effectively copying the file.
+// It sets up error event listeners on both the source and destination streams to handle any errors that may occur during the copy process.
+// It sets up a finish event listener on the destination stream to log a success message when the copy is complete 
+// */
+// readStream.pipe(writeStream);
+
+// readStream.on("error", (err) => {
+// 	console.log("Error reading file", err.message);
+// });
+
+// readStream.on("end", () => {
+// 	console.log("File reading completed");
+// });
+
+// writeStream.on("error", (err) => {
+// 	console.log("Error writing file", err);
+// });
 
 /* --------------------------------------------------------------------------------------------------------------------------------- */
